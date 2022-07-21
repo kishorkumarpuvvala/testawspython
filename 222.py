32bcbdd8cf110b794ec60bd17eef99659deebb9b
kishorkumarpuvvala <94699393+kishorkumarpuvvala@users.noreply.github.com>
2022-07-21 11:59:08 +0530

    Update 222.py

--- 222.py
+++ 222.py
@@ -1,3 +1,48 @@
-i=int(input(print("enter no :")))
-for s in range(1,11):
-	print(i,"X",s,"=",i*s,end="|")
+
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<div class="w3-container">
+  <p>Sprint planning 1st Release with below features:</p>
+  <div class="w3-card-4" style="width:25%;">
+    <header class="w3-container w3-green">
+      <h1>Sprint 1 : Login </h1>
+ </div>
+ <div>
+ <p>Sprint planning 2nd Release with below features:</p>
+  <div class="w3-card-4" style="width:25%;">
+    <header class="w3-container w3-blue">
+      <h1>Sprint 2 : Login  +     Funds transfer  </h1>
+    </header>
+      </div>
+    
+<div>
+ <p>Sprint planning 2nd Release with below features:</p>
+  <div class="w3-card-4" style="width:25%;">
+    <header class="w3-container w3-Oranange">
+      <h1>Sprint 3 : Login  +     Funds transfer  + Manage beneficiary  </h1>
+    </header>
+      </div>
+<div>
+ <p>Sprint planning 2nd Release with below features:</p>
+<div class="w3-card-4" style="width:25%;">
+ <header class="w3-container w3-violet">
+ <h1>Sprint 4 : Login  +     Funds transfer  + Manage beneficiary   +   account balance  </h1>
+    </header>
+      </div>
+<div>
+ <p>Sprint planning 2nd Release with below features:</p>
+<div class="w3-card-4" style="width:25%;">
+ <header class="w3-container w3-violet">
+ <h1>Sprint 5 : Login  +     Funds transfer  + Manage beneficiary   +   account balance   + mmmmmmmmmmm</h1>
+    </header>
+      </div>
+
+  </div>
+
+</body>
+</html>
+
